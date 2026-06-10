4023fec08675c32782acfdd3dc9a17329e30693a
pipe <pipe.duoc@gmail.com>
2026-06-10 19:03:40 -0400

    estructura base de las opciones

--- ejercicio_integrador.py
+++ ejercicio_integrador.py
@@ -1,9 +1,28 @@
 estudiantes = {}
+while True:
+    print("""
+    1. Agregar estudiante
+    2. Agregar nota
+    3. Mostrar estudiantes
+    4. Mostrar promedio de un estudiante
+    5. Mostrar todos los promedios
+    6. Salir""")
 
-print("""
-1. Agregar estudiante
-2. Agregar nota
-3. Mostrar estudiantes
-4. Mostrar promedio de un estudiante
-5. Mostrar todos los promedios
-6. Salir""")
+    opc = input("Seleccione una opción: ").strip()
+
+    if opc == "1":
+        pass
+    elif opc == "2":
+        pass
+    elif opc == "3":
+        pass
+    elif opc == "4":
+        pass
+    elif opc == "5":
+        pass
+    elif opc == "6":
+        print("Gracias por usar el programa")
+        break
+    else:
+        print("ERROR!, Debe ingresar una opcion entre (1-6)")
+    
