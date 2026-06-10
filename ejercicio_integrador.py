2259f94e2b023b5971c78463975cf7910de9d448
pipe <pipe.duoc@gmail.com>
2026-06-10 19:32:25 -0400

    ejercicio finalizado

--- ejercicio_integrador.py
+++ ejercicio_integrador.py
@@ -11,15 +11,56 @@ while True:
     opc = input("Seleccione una opción: ").strip()
 
     if opc == "1":
-        pass
+        nombre = input("Nombre del estudiante: ").strip().title()
+        if nombre in estudiantes:
+            print("Error: El estudiante ya existe.")
+        else:
+            estudiantes[nombre] = []
+            print(f"{nombre} agregado correctamente.")
     elif opc == "2":
-        pass
+        nombre = input("Nombre del estudiante: ").strip().title()
+        if nombre not in estudiantes:
+            print("Error: Estudiante no encontrado.")
+        else:
+            try:
+                nota = float(input("Ingrese la nota: ").strip())
+
+                if 1.0 <= nota <= 7.0:
+                    estudiantes[nombre].append(nota)
+                    print("Nota agregada correctamente.")
+                else:
+                    print("Error: La nota debe estar entre 1.0 y 7.0.")
+
+            except ValueError:
+                print("Error: Debe ingresar una nota válida.")
     elif opc == "3":
-        pass
+        if len(estudiantes) == 0:
+            print("No hay estudiantes registrados.")
+        else:
+            print("\nEstudiantes:")
+            for nombre in estudiantes:
+                print(nombre)
     elif opc == "4":
-        pass
+        nombre = input("Nombre del estudiante: ").strip().title()
+
+        if nombre not in estudiantes:
+            print("Error: Estudiante no encontrado.")
+        elif len(estudiantes[nombre]) == 0:
+            print("El estudiante no tiene notas.")
+        else:
+            promedio = sum(estudiantes[nombre]) / len(estudiantes[nombre])
+            print(f"{nombre} -> Promedio: {promedio:.2f}")
     elif opc == "5":
-        pass
+        if len(estudiantes) == 0:
+            print("No hay estudiantes registrados.")
+        else:
+            print("\nPromedios:")
+            for nombre, notas in estudiantes.items():
+                if len(notas) > 0:
+                    promedio = sum(notas) / len(notas)
+                    print(f"{nombre} -> {promedio:.2f}")
+                else:
+                    print(f"{nombre} -> Sin notas")
     elif opc == "6":
         print("Gracias por usar el programa")
         break
