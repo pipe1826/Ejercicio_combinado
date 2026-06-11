bd7ba74148416ae07e05c93f89c025518bcb6bcc
pipe <pipe.duoc@gmail.com>
2026-06-10 20:02:46 -0400

    ejercicio uno, listas finalizado

--- ejercicio_listas.py
+++ ejercicio_listas.py
@@ -1,6 +1,18 @@
+#ejercicio 1
 numeros = []
 
 for x in range (5):
-    numero = int(input(f"Ingrese el {x+1} número: "))
-    numeros.append(numero)
-    print(numeros)
+    while True:
+        try:
+            numero = int(input(f"Ingrese el {x+1} número: "))
+            numeros.append(numero)
+            break
+        except:
+            print("Debe ingresar un numero entero")
+suma = sum(numeros)
+promedio = suma / len(numeros)
+("\nNúmeros ingresados:")
+for r in numeros.values():
+    print(r)
+print("Suma:", suma)
+print("Promedio:", promedio)
