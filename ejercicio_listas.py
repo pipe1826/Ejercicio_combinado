2eca84c33a7976bbb5c9b5afafe6f6286a0edfc6
pipe1826 <fe.fuentealbaa@duocuc.cl>
2026-06-15 18:06:55 -0400

    Add files via upload

--- ejercicio_listas.py
+++ ejercicio_listas.py
@@ -1,16 +1,29 @@
 #ejercicio 1
-numeros = []
+def pedir_numeros(cantidad):
+    numeros = []
+    for x in range(cantidad):
+        while True:
+            try:
+                numero = int(input(f"Ingrese el {x+1}° número: "))
+                numeros.append(numero)
+                break
+            except ValueError:
+                print("Debe ingresar un número entero.")
 
-for x in range (5):
-    while True:
-        try:
-            numero = int(input(f"Ingrese el {x+1} número: "))
-            numeros.append(numero)
-            break
-        except:
-            print("Debe ingresar un numero entero")
-suma = sum(numeros)
-promedio = suma / len(numeros)
-print("\nNúmeros ingresados:", numeros)
-print("Suma:", suma)
-print("Promedio:", promedio)
+    return numeros
+def calcular_suma_y_promedio(numeros):
+    suma = sum(numeros)
+    promedio = suma / len(numeros)
+    return suma, promedio
+
+def mostrar_resultados(numeros, suma, promedio):
+    print("\nNúmeros ingresados:", numeros)
+    print("Suma:", suma)
+    print("Promedio:", promedio)
+
+def main():
+    numeros = pedir_numeros(5)
+    suma, promedio = calcular_suma_y_promedio(numeros)
+    mostrar_resultados(numeros, suma, promedio)
+
+main()
