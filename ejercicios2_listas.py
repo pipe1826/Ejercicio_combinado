8a461c02538ebdfad7ca5b418ef6b7e295eaddc2
pipe1826 <fe.fuentealbaa@duocuc.cl>
2026-06-15 17:56:43 -0400

    Add files via upload

--- ejercicios2_listas.py
+++ ejercicios2_listas.py
@@ -1,25 +1,42 @@
-numeros = []
+def pedir_numeros(cantidad):
+    numeros = []
 
-for x in range(8):
-    while True:
-        try:
-            numero = int(input(f"Ingrese el {x+1} número a registrar: "))
-            numeros.append(numero)
-            break
-        except ValueError:
-            print("Error: Debe ingresar un número entero.")
+    for x in range(cantidad):
+        while True:
+            try:
+                numero = int(input(f"Ingrese el {x+1}° número a registrar: "))
+                numeros.append(numero)
+                break
+            except ValueError:
+                print("Error: Debe ingresar un número entero.")
 
-mayor = numeros[0]
-menor = numeros[0]
+    return numeros
 
-for numero in numeros:
-    if numero > mayor:
-        mayor = numero
 
-    if numero < menor:
-        menor = numero
+def encontrar_mayor_menor(numeros):
+    mayor = numeros[0]
+    menor = numeros[0]
 
-print("\nLista:", numeros)
-print("Número mayor:", mayor)
-print("Número menor:", menor)
-print("Cantidad de elementos:", len(numeros))
+    for numero in numeros:
+        if numero > mayor:
+            mayor = numero
+        if numero < menor:
+            menor = numero
+
+    return mayor, menor
+
+
+def mostrar_resultados(numeros, mayor, menor):
+    print("\nLista:", numeros)
+    print("Número mayor:", mayor)
+    print("Número menor:", menor)
+    print("Cantidad de elementos:", len(numeros))
+
+
+def main():
+    numeros = pedir_numeros(8)
+    mayor, menor = encontrar_mayor_menor(numeros)
+    mostrar_resultados(numeros, mayor, menor)
+
+
+main()
